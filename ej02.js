fdcaadda18372ba92c8c42f5abbc5ce311fe63ae
Katia <katiaubedam@gmail.com>
2022-07-26 21:53:02 +0200

    cambios

--- ej02.js
+++ ej02.js
@@ -1,19 +1,18 @@
 class Distancia {
-    constructor(valor) {
-        this.valor = valor
+    constructor() {
         this.milla = 1609.34
     }
 
-    MillasAMetros() {
-        return `${this.valor} millas son ${this.valor * this.milla} metros`
+    MillasAMetros(valor) {
+        return `${valor} millas son ${valor * this.milla} metros`
     }
 
-    MillasAKilometros() {
-        return `${this.valor} millas son ${this.valor * (this.milla/1000)} kilómetros`
+    MillasAKilometros(valor) {
+        return `${valor} millas son ${valor * (this.milla/1000)} kilómetros`
     }
 }
 
-let miClase = new Distancia(34)
+let miClase = new Distancia()
 
-console.log(miClase.MillasAMetros())
-console.log(miClase.MillasAKilometros())
+console.log(miClase.MillasAMetros(56))
+console.log(miClase.MillasAKilometros(23))
